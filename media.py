91d4feaae3b3e2e3a29073514b6a9dc2f2336767
Ana Carvalho <anacarvalho@MacBook-Pro-de-Ana.local>
2025-02-19 15:36:39 -0300

    Corrigindo texto de exibição dentro do input

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 # 2 Fazer um programa para receber 3 valores inteiros do usário e mostrar a sua média
 
 num1 = int(input("Insira o primeiro número:\n"))
-num2 = int(input("Insira o primeiro número:\n"))
-num3 = int(input("Insira o primeiro número:\n"))
+num2 = int(input("Insira o segundo número:\n"))
+num3 = int(input("Insira o terceiro número:\n"))
 
 med = (num1 + num2 + num3) / 3
 
